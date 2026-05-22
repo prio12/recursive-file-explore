2f955379bc371cf78922db93d70ebdaf7f1a9112
prio12 <maksudurprio.mp@gmail.com>
2026-05-22 23:51:04 +0600

    feat: added responsive navbar and also kept a button to clear the localstorage and kept the hamburger menu hidden from the md and lg screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-        <h1 className="text-2xl text-red-400">Mini File Explorer</h1>
+      {/* navbar */}
+      <Navbar/>
     </div>
   );
 }
