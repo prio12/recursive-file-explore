733926b7c0dd135d38a2ffed2e4a8b45cc353dd3
prio12 <maksudurprio.mp@gmail.com>
2026-05-23 04:05:39 +0600

    build recursive SidebarItem component to render nested file trees (initially

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Navbar } from "@/components/Navbar";
 import { FileNode } from "@/types/types";
 import { useEffect, useState } from "react";
 import { initialFileSystem } from "../mockData";
+import { SidebarItem } from "@/components/SidebarItem";
+import { findNodeById } from "@/utils/fileUtils";
 
 export default function Home() {
   // Initializing the file tree state with injected dummy data
@@ -10,6 +12,9 @@ export default function Home() {
 
   // Ensures client-only code/localStorage runs only after the initial mount
   const [hasMounted, setHasMounted] = useState<boolean>(false);
+
+  // Tracks the active folder ID, starting at "root" from dummy data and updating based on user clicks
+  const [currentFolderId, setCurrentFolderId] = useState<string>("root");
 
   //Handle initial data load safely on client mount
   useEffect(() => {
@@ -34,10 +39,29 @@ export default function Home() {
     localStorage.setItem("file_tree", JSON.stringify(fileTree));
   }, [fileTree, hasMounted]);
 
+  // Tracks which folder is clicked by the user to update the main workspace view
+  const handleSelectFolder = (id: string) => {
+    setCurrentFolderId(id);
+    // setIsMobileSidebarOpen(false);
+  };
+
+  //Get current folder
+  const activeFolder = findNodeById(fileTree, currentFolderId) || fileTree[0];
+
   return (
     <div className="flex flex-col h-screen bg-slate-50 text-slate-800  overflow-hidden">
       {/* navbar */}
       <Navbar />
+      <div>
+        {fileTree?.map((rootNode) => (
+          <SidebarItem
+            key={rootNode?.id}
+            node={rootNode}
+            currentFolderId={currentFolderId}
+            onSelectFolder={handleSelectFolder}
+          />
+        ))}
+      </div>
     </div>
   );
 }
