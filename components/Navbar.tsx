6a80ce98badd44374d6a6ba78ea55ea09b847b40
prio12 <maksudurprio.mp@gmail.com>
2026-05-23 06:53:24 +0600

    fix: fix hamburger menu mobile toggle and optimize animation smoothness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,19 @@
-import { FolderGit2, Menu, RefreshCw } from "lucide-react";
+import { Menu } from "lucide-react";
 
-export const Navbar = () => {
+interface NavbarItemProps {
+  isMobileSidebarOpen: boolean;
+  setIsMobileSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Navbar = ({
+  isMobileSidebarOpen,
+  setIsMobileSidebarOpen,
+}: NavbarItemProps) => {
   return (
     <header className="flex items-center justify-between h-14 px-4 md:px-6 bg-white border-b border-slate-200 z-30 shadow-sm">
       <div className="flex items-center gap-3">
         <button
-          // onClick={() => setIsMobileSidebarOpen(!isMobileSidebarOpen)}
+          onClick={() => setIsMobileSidebarOpen(!isMobileSidebarOpen)}
           className="p-1.5 hover:bg-slate-100 rounded-md md:hidden text-slate-600 transition"
           aria-label="Toggle directory menu"
         >
@@ -14,18 +22,18 @@ export const Navbar = () => {
 
         <div className="flex items-center gap-2">
           <span className="text-xl">📁</span>
+
           <h1 className="font-bold text-slate-900 tracking-tight text-sm md:text-base">
             Mini File Explorer
           </h1>
         </div>
       </div>
+
       <div className="flex items-center gap-2">
-        <button
-          // onClick={handleResetStorage}
-          className="text-[10px] bg-red-50 cursor-pointer hover:bg-red-100 text-red-600 border border-red-100 px-2 py-1 rounded-md font-medium transition"
-        >
+        <button className="text-[10px] bg-red-50 cursor-pointer hover:bg-red-100 text-red-600 border border-red-100 px-2 py-1 rounded-md font-medium transition">
           Reset Data
         </button>
+
         <div className="text-[10px] md:text-xs bg-green-50 text-green-700 border border-green-100 px-2.5 py-1 rounded-md font-medium">
           Storage Engine Active
         </div>
